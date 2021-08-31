a2d2c3b66980f9c0606208e61df10e765118e96b
wouter.vanletht@pong.nl <wouter.vanletht@pong.nl>
2021-08-31 12:33:32 +0000

    test

---
+++ models/staging/stripe/stg_payments.sql
@@ -0,0 +1,16 @@
+with payment as (
+    
+    select
+    id as payment_id,
+    orderid as order_id,
+    paymentmethod as payment_method,
+    status,
+
+    -- amount is stored in cents, convert it to dollars
+    amount / 100 as amount,
+    created as created_at
+
+    from raw.stripe.payment 
+)
+
+select * from payment
