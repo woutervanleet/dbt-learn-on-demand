88b5fcf38a091160cbf6d23742cfd9d0adae7817
wouter.vanletht@pong.nl <wouter.vanletht@pong.nl>
2021-09-02 13:37:58 +0000

    updated, test, docs and sources

--- models/staging/jaffle_shop/stg_customers.sql
+++ models/staging/jaffle_shop/stg_customers.sql
@@ -1,11 +1,5 @@
-with customers as (
-    
-    select 
-        id as customer_id,
-        first_name,
-        last_name
-
-    from raw.jaffle_shop.customers
-)
-
-select * from customers
+select 
+    id as customer_id,
+    first_name,
+    last_name
+from {{ source('jaffle_shop', 'customers') }}
--- models/staging/stripe/stg_payments.sql
+++ models/staging/stripe/stg_payments.sql
@@ -1,16 +1,9 @@
-with payment as (
-    
-    select
+select
     id as payment_id,
     orderid as order_id,
     paymentmethod as payment_method,
     status,
-
     -- amount is stored in cents, convert it to dollars
     amount / 100 as amount,
     created as created_at
-
-    from raw.stripe.payment 
-)
-
-select * from payment
+from {{ source('stripe', 'payment') }}
